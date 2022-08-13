a87bb392ebace152dd717fb3843e4a0ced11c8de
romariotee <111175904+romariotee@users.noreply.github.com>
2022-08-13 07:42:27 -0400

    Update Javascript.js

--- js/Javascript.js
+++ js/Javascript.js
@@ -12,24 +12,3 @@ navToggle.addEventListener("click",() => {
 		navToggle.setAttribute("aria-expanded", "false");
 	}
 });
-
-<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-		<script>
-		const swiper = new Swiper('.swiper', {
-    
-  loop: true,
-
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
- 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
- 
-});
-		</script>
